5364f2c920621a3b15bf02f528db5945e59081be
Aldanard <bolvarfordragon296@seznam.cz>
2020-11-17 21:20:06 +0100

    finalni commit

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const tlacitko = document.getElementById('vypocetMeny');
+const tlacitko = document.getElementById('tlacitko');
 tlacitko.addEventListener('click',function() {
     let value = parseFloat(document.getElementById('stranaA').value);
 console.log(value);
-function vypocetObvodu(value1){
-    return(value1*4);
+function vypocetObvodu(value){
+    console.log(value*4);
+    return(value*4);
+
 }
-function vypocetObsahu(value2){
-    return(value2*value2);
+function vypocetObsahu(value){
+
+    console.log(value*value);
+    return(value*value);
+ 
     
 }
-document.getElementById('popok').innerHTML = "<p>" + vypocetObvodu(value1).toFixed(2) + "cm </p>";
+document.getElementById('papak').innerHTML = "<p> obvod je: " + vypocetObvodu(value).toFixed(2) + "cm </p>";
+document.getElementById('pypyk').innerHTML = "<p> obsah je: " + vypocetObsahu(value).toFixed(2) + "cm² </p>";
+});
